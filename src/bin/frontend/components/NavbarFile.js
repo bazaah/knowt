3b1860208748bf54762095855b1b28fbe5f52255
Baz <github(a)luxolus.com>
2018-10-08 11:07:50 +0000

    added workingFile to onPress

--- src/bin/frontend/components/NavbarFile.js
+++ src/bin/frontend/components/NavbarFile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchContent } from "../redux/actions/contentActions";
+import { fetchContent, workingFile } from "../redux/actions/contentActions";
 import { bindActionCreators } from "redux";
 
 class NavbarFile extends React.Component {
@@ -12,8 +12,12 @@ class NavbarFile extends React.Component {
   }
 
   onPress(fileName) {
-    const path = "api/".concat(this.props.path.slice(1).join("/")).concat("/"); // This is why I hate js array manipulation TODO: improve backend data so I can avoid this
-    this.props.fetchContent(path.concat(fileName));
+    const path = this.props.path
+      .slice(1)
+      .join("/")
+      .concat("/"); // This is why I hate js array manipulation TODO: improve backend data so I can avoid this
+    this.props.workingFile(path.concat(fileName));
+    this.props.fetchContent("api/".concat(path.concat(fileName)));
   }
   render() {
     const name = this.props.file;
@@ -27,7 +31,8 @@ class NavbarFile extends React.Component {
 
 function mapDispatchToProps(dispatch) {
   return {
-    fetchContent: url => dispatch(fetchContent(url))
+    fetchContent: url => dispatch(fetchContent(url)),
+    workingFile: file => dispatch(workingFile(file))
   };
 }
 
