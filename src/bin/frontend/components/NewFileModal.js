4b8f31c2779ff5c8d52380ff3a539f02fefc38b6
Baz <github(a)luxolus.com>
2018-10-08 15:55:44 +0000

    Started adding new file functionality

---
+++ src/bin/frontend/components/NewFileModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { connect } from "react-redux";
+import ReactModal from "react-modal";
+
+ReactModal.setAppElement("#root");
+
+class NewFileModal extends React.Component {}
