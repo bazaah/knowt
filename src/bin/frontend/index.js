9426120126c369b3ba72b417fa49723158f8b7e5
Baz <github(a)luxolus.com>
2018-10-09 15:25:33 +0000

    removed a div

--- src/bin/frontend/index.js
+++ src/bin/frontend/index.js
@@ -7,7 +7,7 @@ import style from "./index.css";
 import Content from "./components/Content";
 import Navbar from "./components/Navbar";
 import Menubar from "./components/Menubar";
-import NewFileModal from "./components/NewFileModal"
+import NewFileModal from "./components/NewFileModal";
 import store from "./redux/store";
 
 class App extends React.Component {
@@ -15,9 +15,7 @@ class App extends React.Component {
     return (
       <Provider store={store}>
         <div className={style.wrapper}>
-        <div>
-        <NewFileModal></NewFileModal>
-        </div>
+          <NewFileModal />
           <div className={style.header}>
             <Menubar />
           </div>
