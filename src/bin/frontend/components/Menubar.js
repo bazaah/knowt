7f91f0776ccc8719f3da20bfb0f7f699decb6da5
Baz <github(a)luxolus.com>
2018-10-09 10:21:13 +0000

    added buttons for modal + base form

--- src/bin/frontend/components/Menubar.js
+++ src/bin/frontend/components/Menubar.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { setBansaFilter } from "../redux/actions/bansaActions";
+import {
+  setBansaFilter,
+  showNewFileModal
+} from "../redux/actions/bansaActions";
 import { connect } from "react-redux";
 
 class Menubar extends React.Component {
@@ -7,16 +10,23 @@ class Menubar extends React.Component {
     super(props);
 
     this.onPress = this.onPress.bind(this);
+    this.handleOpenModal = this.handleOpenModal.bind(this);
   }
 
   onPress(filter) {
     this.props.setBansaFilter(filter);
   }
+
+  handleOpenModal(bool) {
+    this.props.showNewFileModal(bool);
+  }
+
   render() {
     return (
       <div>
         <button onClick={() => this.onPress("MARKDOWN_VIEW")}>Markdown</button>
         <button onClick={() => this.onPress("EDITOR_VIEW")}>Editor</button>
+        <button onClick={() => this.handleOpenModal(true)}>New File</button>
       </div>
     );
   }
@@ -24,7 +34,8 @@ class Menubar extends React.Component {
 
 function mapDispatchToProps(dispatch) {
   return {
-    setBansaFilter: filter => dispatch(setBansaFilter(filter))
+    setBansaFilter: filter => dispatch(setBansaFilter(filter)),
+    showNewFileModal: bool => dispatch(showNewFileModal(bool))
   };
 }
 
--- src/bin/frontend/components/NewFileModal.js
+++ src/bin/frontend/components/NewFileModal.js
@@ -1,7 +1,53 @@
 import React from "react";
 import { connect } from "react-redux";
 import ReactModal from "react-modal";
+import { showNewFileModal } from "../redux/actions/bansaActions";
 
 ReactModal.setAppElement("#root");
 
-class NewFileModal extends React.Component {}
+class NewFileModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleCloseModal(bool) {
+    this.props.showNewFileModal(bool);
+  }
+
+  render() {
+    return (
+      <ReactModal isOpen={this.props.modalVisible}>
+        <form>
+          <label>
+            Path <br />
+            <input type="text" placeholder="path/to/file" />
+          </label>
+          <br />
+          <label>
+            Name <br />
+            <input type="text" placeholder="Coolnewfile" />
+          </label>
+          <br />
+          <button onClick={() => this.handleCloseModal(false)}>Submit</button>
+        </form>
+      </ReactModal>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  modalVisible: state.bansa.modalVisible
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    showNewFileModal: bool => dispatch(showNewFileModal(bool))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NewFileModal);
