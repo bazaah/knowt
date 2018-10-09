1839ff505edb641b508ac8646201f6b07a1fe3b8
Baz <github(a)luxolus.com>
2018-10-09 10:20:08 +0000

    added state for modal visibilty

--- src/bin/frontend/redux/actions/bansaActions.js
+++ src/bin/frontend/redux/actions/bansaActions.js
@@ -1,9 +1,16 @@
 // Bansa is Korean for "reflection"
-import { SET_BANSA_FILTER } from "./types";
+import { SET_BANSA_FILTER, SHOW_NEW_FILE_MODAL } from "./types";
 
 export function setBansaFilter(filter) {
   return {
     type: SET_BANSA_FILTER,
     filter: filter
+  };
+}
+
+export function showNewFileModal(bool) {
+  return {
+    type: SHOW_NEW_FILE_MODAL,
+    visible: bool
   };
 }
--- src/bin/frontend/redux/reducers/bansaReducer.js
+++ src/bin/frontend/redux/reducers/bansaReducer.js
@@ -1,8 +1,9 @@
 // Bansa is Korean for "reflection"
-import { SET_BANSA_FILTER, BansaFilter } from "../actions/types";
+import { SET_BANSA_FILTER, BansaFilter, SHOW_NEW_FILE_MODAL } from "../actions/types";
 
 const initialState = {
-  bansaFilter: BansaFilter.MARKDOWN_VIEW
+  bansaFilter: BansaFilter.MARKDOWN_VIEW,
+  modalVisible: false
 };
 
 export default function(state = initialState, action) {
@@ -12,6 +13,11 @@ export default function(state = initialState, action) {
         ...state,
         bansaFilter: action.filter
       };
+    case SHOW_NEW_FILE_MODAL:
+      return {
+        ...state,
+        modalVisible: action.visible
+      }
     default:
       return state;
   }
