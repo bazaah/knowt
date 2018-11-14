6553b3043e9bd248bdd001ac488ba5cbb4b7937a
Baz <github(a)luxolus.com>
2018-11-14 12:02:45 +0000

    pb value -> pb ref

--- src/bin/backend/proc.rs
+++ src/bin/backend/proc.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
-pub fn yaml_deposit(file: String, path: PathBuf) {
+pub fn yaml_deposit(file: String, path: &PathBuf) {
     let proc_deposit = match Command::new("proc/deposit")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
