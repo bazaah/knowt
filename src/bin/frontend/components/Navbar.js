1fbf63b315689e3163c6e3968ed51c2b5747724f
Baz <github(a)luxolus.com>
2018-11-15 11:29:14 +0000

    improved file display mapping

--- src/bin/frontend/components/Navbar.js
+++ src/bin/frontend/components/Navbar.js
@@ -1,20 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { fetchFileDir } from "../redux/actions/contentActions";
+import React from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import { fetchFileDir } from "../redux/actions/contentActions"
 
 // Subcomponent
-import NavbarFile from "./NavbarFile";
+import NavbarFile from "./NavbarFile"
 
 // Small subcomponent for displaying file parents
 class NavbarFolder extends React.Component {
   render() {
-    const folder = this.props.folder;
+    const folder = this.props.folder
     return (
       <tr>
         <th>{folder}</th>
       </tr>
-    );
+    )
   }
 }
 
@@ -23,32 +23,34 @@ class NavbarFolder extends React.Component {
 class Navbar extends React.Component {
   componentDidMount() {
     // Fetches file tree before first render
-    this.props.fetchFileDir("/api/dir");
+    this.props.fetchFileDir("/api/dir")
   }
 
   render() {
     if (this.props.fileDirLoading) {
-      return <p>Loading...</p>;
+      return <p>Loading...</p>
     }
 
-    const rows = [];
-    let lastFolder = null;
+    const rows = []
+    let lastFolder = null
 
-    // Maps the array of objects like so:
-    // If array exists, map each object in array to 'rows'
-    // If object.parent is not equal to lastFolder, push an instance of NavbarFolder
-    // Push an instance of NavbarFile
-    // Set lastFolder to this object's parent attribute
+    // Maps the array of files:
+    // If array exists, map the array and check if the current parent
+    // is the same as the last parent. If it is not push a header (NavbarFolder),
+    // update lastFolder and run another map, pushing any files with the same parent
     if (this.props.fileList) {
-      this.props.fileList.map(file => {
-        if (file.parent !== lastFolder) {
-          rows.push(<NavbarFolder folder={file.parent} key={file.id} />);
+      this.props.fileList.map(folder => {
+        if (folder.parent !== lastFolder) {
+          rows.push(<NavbarFolder folder={folder.parent} />)
+          lastFolder = folder.parent
+
+          this.props.fileList.map(file => {
+            if (file.parent == lastFolder) {
+              rows.push(<NavbarFile file={file.name} path={file.path} />)
+            }
+          })
         }
-        rows.push(
-          <NavbarFile file={file.name} path={file.path} key={file.id} />
-        );
-        lastFolder = file.parent;
-      });
+      })
     }
 
     return (
@@ -60,7 +62,7 @@ class Navbar extends React.Component {
         </thead>
         <tbody>{rows}</tbody>
       </table>
-    );
+    )
   }
 }
 
@@ -68,28 +70,28 @@ Navbar.propTypes = {
   fileList: PropTypes.array.isRequired,
   fetchFileDir: PropTypes.func.isRequired,
   fileDirLoading: PropTypes.bool
-};
+}
 
 NavbarFolder.propTypes = {
   folder: PropTypes.string
-};
+}
 
 // Subscribes to the store
 const mapStateToProps = state => {
   return {
     fileList: state.content.fileDirList.result,
     fileDirLoading: state.content.fileDirIsLoading
-  };
-};
+  }
+}
 
 //binds action creators to the indicated prop object
 function mapDispatchToProps(dispatch) {
   return {
     fetchFileDir: url => dispatch(fetchFileDir(url))
-  };
+  }
 }
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Navbar);
+)(Navbar)
