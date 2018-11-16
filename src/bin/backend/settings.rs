1771f6a539e33c1fa1067449ae3ceede670e6970
Baz <github(a)luxolus.com>
2018-11-16 14:29:39 +0000

    Begun integration of rocket settings -> clap

--- src/bin/backend/settings.rs
+++ src/bin/backend/settings.rs
@@ -1,5 +1,6 @@
-use clap::{App, Arg};
-use config::{Config, File};
+use clap::{App, Arg, SubCommand};
+use config::{Config as FileConfig, File};
+use rocket::config::{Config, Environment};
 use std::path::PathBuf;
 use std::sync::RwLock;
 
@@ -37,7 +38,7 @@ impl Settings {
     }
 }
 
-pub fn init_clap() {
+pub fn init_clap() -> Option<Config> {
     let matches = App::new("Knowt")
         .about("Knowt Alpha")
         .author(crate_authors!("\n"))
@@ -49,17 +50,44 @@ pub fn init_clap() {
                 .help("Set your custom config file")
                 .takes_value(true),
         )
+        .subcommand(
+            SubCommand::with_name("rocket")
+                .about("Controls Rocket configuration")
+                .arg(
+                    Arg::with_name("env")
+                        .short("E")
+                        .long("env")
+                        .takes_value(true)
+                        .possible_values(&["dev", "stage", "prod"]),
+                ),
+        )
         .get_matches();
 
+    let mut application: Option<Config> = None;
+    if let Some(rocket) = matches.subcommand_matches("rocket") {
+        if rocket.is_present("env") {
+            match rocket.value_of("env") {
+                Some("dev") => application = Some(Config::development().expect("Bad CWD")),
+                Some("stage") => application = Some(Config::staging().expect("Bad CWD")),
+                Some("prod") => application = Some(Config::production().expect("Bad CWD")),
+                None => println!("Whoopsie"),
+                _ => println!("The great void beckons"),
+            }
+        }
+    }
+
+    /* --- */
     let config_path = matches
         .value_of("config")
         .unwrap_or("example/config/settings");
-    let mut config = Config::new();
-    config.merge(File::with_name(config_path)).unwrap();
+    let mut config_file = FileConfig::new();
+    config_file.merge(File::with_name(config_path)).unwrap();
 
-    let srv_root = PathBuf::from(config.get_str("path").unwrap());
-    let stc_cnt = PathBuf::from(config.get_str("static_content").unwrap());
+    let srv_root = PathBuf::from(config_file.get_str("path").unwrap());
+    let stc_cnt = PathBuf::from(config_file.get_str("static_content").unwrap());
 
     SETTINGS.write().unwrap().set_server_root(srv_root);
     SETTINGS.write().unwrap().set_static_content(stc_cnt);
+
+    return application;
 }
