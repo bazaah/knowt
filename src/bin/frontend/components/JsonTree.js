7e38272e6b6dc8591e98c2e7c5d8515f5652ed0c
Baz <github(a)luxolus.com>
2019-02-04 09:21:40 +0000

    WIP, added handleSelect, redux dispatch
    The Tree viewer now correctly determines if the field that is clicked on
    is a json pointer to a large (Markdown) field and handles it accordingly
    
    Edit functionality for non pointer fields is next on the agenda
    ---BROKEN---

--- src/bin/frontend/components/JsonTree.js
+++ src/bin/frontend/components/JsonTree.js
@@ -2,31 +2,64 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import JsonViewer from "react-json-view";
+import {
+  workingFileElement,
+  fetchElement
+} from "../redux/actions/contentActions";
 
 class JsonTree extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSelect.bind(this);
+  }
+
+  handleSelect(callback) {
+    const regex = /\{\{((\/[a-zA-Z0-9-_]+)+)\}\}/g;
+    if (regex.test(callback.value)) {
+      let jpointer = callback.value.replace(regex, "$2");
+      console.log(jpointer);
+      this.props.workingFileElement(jpointer);
+      console.log(this.props.workingFile.pointer);
+      this.props.fetchElement(this.props.workingFile.path, jpointer);
+    }
+  }
   render() {
     return (
       <JsonViewer
         src={this.props.file}
         collapseStringsAfterLength={12}
         name={false}
-        indentWidth={2}
+        indentWidth={1}
         displayDataTypes={false}
         displayObjectSize={false}
+        onEdit={object => {
+          console.log(object);
+        }}
+        onSelect={select => this.handleSelect(select)}
       />
     );
   }
 }
 
 JsonTree.propTypes = {
-  file: PropTypes.object
+  file: PropTypes.object,
+  workingFileElement: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
-  file: state.content.contentData.result
+  file: state.content.contentData.result,
+  workingFile: state.content.workingFile
 });
+
+function mapDispatchToProps(dispatch) {
+  return {
+    workingFileElement: pointer => dispatch(workingFileElement(pointer)),
+    fetchElement: (path, pointer) => dispatch(fetchElement(path, pointer))
+  };
+}
 
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(JsonTree);
