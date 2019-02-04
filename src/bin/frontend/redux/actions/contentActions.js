ee88750767e8c374a39025a44314f16c51686a4c
Baz <github(a)luxolus.com>
2019-02-04 09:34:56 +0000

    Added fetchElement + related actions
    fetchElement is unstable, haven't nailed down URL query handling yet
    ---BROKEN---

--- src/bin/frontend/redux/actions/contentActions.js
+++ src/bin/frontend/redux/actions/contentActions.js
@@ -11,22 +11,26 @@ import {
   NEW_FILE_ERROR,
   NEW_FILE_LOADING,
   NEW_FILE_SUCCESS,
-  WORKING_FILE
-} from "./types"
+  ELEMENT_HAS_ERROR,
+  ELEMENT_IS_LOADING,
+  ELEMENT_FETCH_SUCCESS,
+  WORKING_FILE_PATH,
+  WORKING_FILE_ELEMENT
+} from "./types";
 
 // Async thunk meta action for file tree population
 // Returns an array of objects
 export function fetchFileDir(url) {
   return dispatch => {
-    dispatch(fileDirLoading(true))
+    dispatch(fileDirLoading(true));
 
     fetch(url)
       .then(res => {
-        dispatch(fileDirLoading(false))
-        return res.json()
+        dispatch(fileDirLoading(false));
+        return res.json();
       })
-      .then(data => dispatch(fileDirFetchSuccess(data)))
-  }
+      .then(data => dispatch(fileDirFetchSuccess(data)));
+  };
 }
 
 // Async thunk meta action for fetching the content
@@ -34,20 +38,19 @@ export function fetchFileDir(url) {
 // Returns a json object
 export function fetchContent(url) {
   return dispatch => {
-    dispatch(contentLoading(true))
-
+    dispatch(contentLoading(true));
     fetch(url)
       .then(res => {
         // this doesn't work, investigate correct method
         if (res.status !== 200) {
-          throw Error(res.result)
+          throw Error(res.result);
         }
-        dispatch(contentLoading(false))
-        return res.json()
+        dispatch(contentLoading(false));
+        return res.json();
       })
       .then(data => dispatch(contentFetchSuccess(data)))
-      .catch(() => dispatch(contentError(true)))
-  }
+      .catch(() => dispatch(contentError(true)));
+  };
 }
 
 // Async thunk meta action for requesting an update
@@ -55,7 +58,7 @@ export function fetchContent(url) {
 // Returns a json object
 export function updateContent(url, updateData) {
   return dispatch => {
-    dispatch(updateLoading(true))
+    dispatch(updateLoading(true));
     fetch(url, {
       method: "PUT",
       headers: {
@@ -65,14 +68,14 @@ export function updateContent(url, updateData) {
     })
       .then(res => {
         if (res.status !== 200) {
-          throw Error(res.result)
+          throw Error(res.result);
         }
-        dispatch(updateLoading(false))
-        return res.json()
+        dispatch(updateLoading(false));
+        return res.json();
       })
       .then(data => dispatch(updateSuccess(data)))
-      .catch(() => dispatch(updateError(true)))
-  }
+      .catch(() => dispatch(updateError(true)));
+  };
 }
 
 // Async thunk meta action for creating a file,
@@ -80,7 +83,7 @@ export function updateContent(url, updateData) {
 // Returns a status code
 export function newContent(url, newFile) {
   return dispatch => {
-    dispatch(newFileLoading(true))
+    dispatch(newFileLoading(true));
     fetch(url, {
       method: "POST",
       headers: {
@@ -90,22 +93,50 @@ export function newContent(url, newFile) {
     })
       .then(res => {
         if (res.status !== 200) {
-          throw Error(res.status)
+          throw Error(res.status);
         }
-        dispatch(newFileLoading(false))
-        return res.json()
+        dispatch(newFileLoading(false));
+        return res.json();
       })
       .then(code => dispatch(newFileSuccess(code)))
-      .catch(() => dispatch(updateError(true)))
-  }
+      .catch(() => dispatch(updateError(true)));
+  };
 }
 
-// Action for setting the current working file
-export function workingFile(file) {
+export function fetchElement(path, pointer) {
+  return dispatch => {
+    const url = new URL("api/vfield", "http://172.17.17.223:8001");
+    let params = { file_path: path, pointer_path: pointer };
+    url.search = new URLSearchParams(params);
+    dispatch(elementFetchLoading(true));
+    fetch(url, {
+      method: "POST"
+    })
+      .then(res => {
+        if (res.status !== 200) {
+          throw Error(res.status);
+        }
+        dispatch(elementFetchLoading(false));
+        return res.json();
+      })
+      .then(element => dispatch(elementFetchSuccess(element)))
+      .catch(() => dispatch(elementFetchError(true)));
+  };
+}
+
+// Actions for setting the current working file and element
+export function workingFilePath(path) {
   return {
-    type: WORKING_FILE,
-    file: file
-  }
+    type: WORKING_FILE_PATH,
+    path: path
+  };
+}
+
+export function workingFileElement(pointer) {
+  return {
+    type: WORKING_FILE_ELEMENT,
+    pointer: pointer
+  };
 }
 
 // Action creators used in the meta actions
@@ -113,82 +144,103 @@ export function fileDirError(bool) {
   return {
     type: FILEDIR_HAS_ERROR,
     error: bool
-  }
+  };
 }
 
 export function fileDirLoading(bool) {
   return {
     type: FILEDIR_IS_LOADING,
     loading: bool
-  }
+  };
 }
 
 export function fileDirFetchSuccess(fileDir) {
   return {
     type: FILEDIR_FETCH_SUCCESS,
     payload: fileDir
-  }
+  };
 }
 
 export function contentError(bool) {
   return {
     type: CONTENT_HAS_ERROR,
     error: bool
-  }
+  };
 }
 
 export function contentLoading(bool) {
   return {
     type: CONTENT_IS_LOADING,
     loading: bool
-  }
+  };
 }
 
 export function contentFetchSuccess(content) {
   return {
     type: CONTENT_FETCH_SUCCESS,
     payload: content
-  }
+  };
 }
 
 export function updateError(bool) {
   return {
     type: UPDATE_ERROR,
     error: bool
-  }
+  };
 }
 
 export function updateLoading(bool) {
   return {
     type: UPDATE_LOADING,
     loading: bool
-  }
+  };
 }
 
 export function updateSuccess(update) {
   return {
     type: UPDATE_SUCCESS,
     payload: update
-  }
+  };
 }
 
 export function newFileError(bool) {
   return {
     type: NEW_FILE_ERROR,
     error: bool
-  }
+  };
 }
 
 export function newFileLoading(bool) {
   return {
     type: NEW_FILE_LOADING,
     loading: bool
-  }
+  };
 }
 
 export function newFileSuccess(code) {
   return {
     type: NEW_FILE_SUCCESS,
     status: code
-  }
+  };
+}
+
+export function elementFetchError(bool) {
+  return {
+    type: ELEMENT_HAS_ERROR,
+    error: bool
+  };
+}
+
+export function elementFetchLoading(bool) {
+  return {
+    type: ELEMENT_IS_LOADING,
+    loading: bool
+  };
+}
+
+export function elementFetchSuccess(element) {
+  return {
+    type: ELEMENT_FETCH_SUCCESS,
+    payload: element
+  };
 }
